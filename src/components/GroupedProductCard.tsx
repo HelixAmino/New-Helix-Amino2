b7595cc6aa343a1aea66c0ee0facf954bf694729
Helix-Amino-Research <matt@helixamino.com>
2026-04-16 00:02:24 -0400

    Fix GroupedProductCard image scaling

--- src/components/GroupedProductCard.tsx
+++ src/components/GroupedProductCard.tsx
@@ -35,11 +35,11 @@ export function GroupedProductCard({ group }: Props) {
       onClick={handleClick}
     >
       {/* Image */}
-      <div className="relative h-[480px] overflow-hidden bg-[#07111d]">
+      <div className="relative h-56 overflow-hidden bg-[#07111d]">
         <img
           src={group.image}
           alt={group.baseName}
-          className="w-full h-full object-contain group-hover:scale-110 transition-transform duration-500"
+          className="absolute inset-0 w-full h-full object-contain scale-[1.15] group-hover:scale-[1.25] transition-transform duration-500"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-[#07111d]/60 via-transparent to-transparent" />
 
